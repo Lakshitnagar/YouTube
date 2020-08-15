e9e702898b154a4bd1dde0d4e34b169de3feb1a6
Lakshit Nagar <lakshitnagar@gmail.com>
2020-08-16 02:02:39 +0530

    image mocking

--- src/__tests__/components/AppLogo/AppLogo.test.js
+++ src/__tests__/components/AppLogo/AppLogo.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {shallow} from "enzyme";
 import {shallowToJson} from "enzyme-to-json";
-
 import AppLogo from "../../../components/AppLogo/AppLogo";
+
+jest.mock('../../../assets/images/logo.svg', () => 'mockLogoIcon');
 
 describe('AppLogo', function () {
     it('should render and match snapshot', function () {
--- src/__tests__/components/SearchForm/SearchForm.test.js
+++ src/__tests__/components/SearchForm/SearchForm.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {shallow} from "enzyme";
 import {shallowToJson} from "enzyme-to-json";
-
 import SearchForm from "../../../components/SearchForm/SearchForm";
+
+jest.mock('../../../assets/images/search_18dp.svg', () => 'mockSearchIcon');
 
 describe('SearchForm', function () {
     it('should render and match snapshot', function () {
