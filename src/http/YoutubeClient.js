a19d33cab5d82b869a01cdbecf1eee74649417a3
Lakshit Nagar <lakshitnagar@gmail.com>
2020-08-16 15:51:45 +0530

    Adding api key and query

--- src/http/YoutubeClient.js
+++ src/http/YoutubeClient.js
@@ -1,8 +1,14 @@
 import {get} from './Client';
 import {getYTSearchUrl} from "../helpers/UrlHelper";
 
+const searchByKeywordParams = {
+    part: 'snippet',
+    maxResults: 25,
+    key: 'AIzaSyAJ2wbkcR5-AMEuy4orWFyOOaMnb5ETxns'
+};
+
 export async function searchByKeyword(keyword) {
     const YTSearchUrl = getYTSearchUrl();
-    const res = await get(YTSearchUrl);
+    const res = await get(YTSearchUrl, {...searchByKeywordParams, ...{q: keyword}});
     return res.data;
 }
