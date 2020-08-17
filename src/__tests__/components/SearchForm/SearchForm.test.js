37dae9926a4859ba793f1829ad538e85ad747087
Lakshit Nagar <lakshitnagar@gmail.com>
2020-08-17 22:24:31 +0530

    search from input field

--- src/__tests__/components/SearchForm/SearchForm.test.js
+++ src/__tests__/components/SearchForm/SearchForm.test.js
@@ -1,16 +1,89 @@
-import React from 'react';
+import React, {useState as useStateMock} from 'react';
 import {shallow} from "enzyme";
 import {shallowToJson} from "enzyme-to-json";
-import SearchForm from "../../../components/SearchForm/SearchForm";
+import {mapDispatchToProps, SearchForm} from "../../../components/SearchForm/SearchForm";
+import * as YouTubeActions from "../../../store/actions/YouTubeActions";
 
 jest.mock('../../../assets/images/search_18dp.svg', () => 'mockSearchIcon');
+const mockFetchYouTubeVideosByKeyword = jest.fn();
+const mockSetSearchKeyword = jest.fn();
+const mockPreventDefault = jest.fn();
+
+jest.mock("react", () => {
+    return {
+        ...jest.requireActual("react"),
+        useState: jest.fn()
+    }
+});
 
 describe('SearchForm', function () {
+
+    beforeEach(() => {
+        useStateMock.mockImplementation(init => [init, mockSetSearchKeyword]);
+    });
+
     it('should render and match snapshot', function () {
-        const component = <SearchForm />;
+        const component = <SearchForm/>;
 
         const wrapper = shallow(component);
 
         expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should save input value in local store on change', function () {
+
+        const component = <SearchForm/>;
+
+        const wrapper = shallow(component);
+
+        wrapper.find('input').simulate('change', {
+            target: {
+                value: 'changedText'
+            }
+        });
+
+        expect(mockSetSearchKeyword).toHaveBeenCalledWith('changedText');
+    });
+
+    it('should not search for youtube videos on form submit when user does not enter search keyword', function () {
+        const component = <SearchForm fetchYouTubeVideosByKeyword={mockFetchYouTubeVideosByKeyword}/>;
+
+        const wrapper = shallow(component);
+
+        wrapper.find('form').simulate('submit', {
+            preventDefault: mockPreventDefault
+        });
+
+        expect(mockPreventDefault).toHaveBeenCalled();
+        expect(mockFetchYouTubeVideosByKeyword).toHaveBeenCalledTimes(0);
+    });
+
+    it('should search for youtube videos on form submit', function () {
+        useStateMock.mockImplementation(init => ['h', mockSetSearchKeyword]);
+
+        const component = <SearchForm fetchYouTubeVideosByKeyword={mockFetchYouTubeVideosByKeyword}/>;
+
+        const wrapper = shallow(component);
+
+        wrapper.find('form').simulate('submit', {
+            preventDefault: mockPreventDefault
+        });
+
+        expect(mockPreventDefault).toHaveBeenCalled();
+        expect(mockFetchYouTubeVideosByKeyword).toHaveBeenCalled();
+    });
+});
+
+describe('mapDispatchToProps', () => {
+    it('should add dispatches to props', () => {
+        const dispatch = jest.fn();
+        spyOn(YouTubeActions, 'fetchYouTubeVideosByKeyword').and.returnValue(mockFetchYouTubeVideosByKeyword);
+
+        const actual = mapDispatchToProps(dispatch);
+
+        actual.fetchYouTubeVideosByKeyword('hello');
+
+        expect(Object.getOwnPropertyNames(actual)).toEqual(['fetchYouTubeVideosByKeyword']);
+        expect(dispatch).toHaveBeenCalledWith(mockFetchYouTubeVideosByKeyword);
     });
 });
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import search from '../../assets/images/search_18dp.svg';
 import './SearchForm.scss';
+import {fetchYouTubeVideosByKeyword} from "../../store/actions/YouTubeActions";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 
-function SearchForm() {
+export function SearchForm(props) {
+    const [searchKeyword, setSearchKeyword] = useState('');
+
+    const onSearchFormSubmit = (e) => {
+        e.preventDefault();
+        if (searchKeyword) props.fetchYouTubeVideosByKeyword(searchKeyword);
+    };
+
     return (
         <div className="searchForm">
-            <form className="searchForm__form">
+            <form className="searchForm__form" onSubmit={(e) => {
+                onSearchFormSubmit(e)
+            }}>
                 <div className="searchForm__inputContainer">
                     <input id="search" autoCapitalize="none" autoComplete="off" autoCorrect="off"
-                           name="search_query"
-                           tabIndex="0" type="text" spellCheck="false" placeholder="Search"/>
+                           type="text" spellCheck="false" placeholder="Search"
+                           onChange={e => setSearchKeyword(e.target.value)}/>
                 </div>
 
                 <button id="search-icon" className="searchForm__submit" aria-label="Search">
@@ -20,4 +32,14 @@ function SearchForm() {
     );
 }
 
-export default SearchForm;
+export const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchYouTubeVideosByKeyword: (keyword) => dispatch(fetchYouTubeVideosByKeyword(keyword))
+    };
+};
+
+SearchForm.propTypes = {
+    fetchYouTubeVideosByKeyword: PropTypes.func,
+};
+
+export default connect(null, mapDispatchToProps)(SearchForm);
